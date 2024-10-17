95f97790f92a0773f2a812e0e8382e590127c4ec
Ayushkumarcoder <ayushrajeshmishra20@gmail.com>
2024-10-17 15:05:17 +0530

    added the login page

--- client/src/UsernamePrompt.jsx
+++ client/src/UsernamePrompt.jsx
@@ -11,5 +11,26 @@ function UsernamePrompt() {
       localStorage.setItem('username', username);
       navigate('/chat');
     }
-  }
+  };
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h2>Login to join the chat</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text"
+        placeholder='username' value={username} 
+        onChange={(e)=> setUsername(e.target.value)} 
+        style={{ padding: '10px', width: '200px', marginRight: '10px' }}
+        />
+        <button type='submit' style={{ padding: '10px' }}>Join Chat</button>
+
+      </form>
+
+
+    </div>
+  );
 }
+
+
+export default UsernamePrompt;
+
